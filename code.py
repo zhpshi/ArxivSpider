07b5d243f1b536191d873aec6af702e7e7e5baa2
zpshi <zhaopshi@gmail.com>
2021-12-25 20:07:05 +0800

    Initialize the program.

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 25/12/2021 8:02 PM
+@Author  : Zhaopeng SHI
+@Email   : zhaopshi@gmail.com
+@File    : code.py
+"""
+
+
+# import package here
+
+def mian()
+
+    return 0
+
+
+if __name__ == '__main__'
+
+    main()
